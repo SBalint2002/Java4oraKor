88369cc9ceb7ee66e57ed2dfc7a4e31144adfb4b
Sárándi <sarandi.balint@gmail.com>
2022-09-15 19:02:58 +0200

    Feladat1

--- src/hu/petrik/koroop/Main.java
+++ src/hu/petrik/koroop/Main.java
@@ -1,2 +1,11 @@
-package hu.petrik.koroop;public class Main {
+package hu.petrik.koroop;
+
+import hu.petrik.koroop.kor.Kor;
+
+public class Main {
+    public static void main(String[] args) {
+        Kor k1 = new Kor(13, 3, 6);
+        Kor k2 = new Kor(6);
+        Kor k3 = new Kor(Math.random()*100, (int)(Math.random()*10)+1, (int)(Math.random()*10)+1);
+    }
 }
--- src/hu/petrik/koroop/kor/Kor.java
+++ src/hu/petrik/koroop/kor/Kor.java
@@ -1,2 +1,35 @@
-package hu.petrik.koroop.kor;public class Kor {
+package hu.petrik.koroop.kor;
+
+public class Kor {
+    private double sugar;
+    private int x;
+    private int y;
+
+    //sugár és pont
+    public Kor(double sugar, int pont1, int pont2) {
+        this.sugar = sugar;
+        this.x = pont1;
+        this.y = pont2;
+    }
+
+    //csak sugár
+    public Kor(double sugar){
+        this.sugar = sugar;
+        x = 0;
+        y = 0;
+    }
+
+    //véletlen
+
+
+    public double getKerulet(){
+        return ((sugar*2)*Math.PI);
+    }
+
+    public double getTerulet(){
+        return ((sugar*2)*Math.PI);
+    }
+
+
+
 }
